62923e188f6c19dad4ba4e0e5eb61a25d905d15b
MatthewHolmesPayble <156634487+MatthewHolmesPayble@users.noreply.github.com>
2024-08-28 14:44:08 +1000

    Change to theme.

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -1,8 +1,11 @@
 return {
-  'rebelot/kanagawa.nvim',
-  name = 'kanagawa',
-  priority = 1001,
-  config = function()
-    vim.cmd.colorscheme 'kanagawa-wave'
-  end
+	"rebelot/kanagawa.nvim",
+	name = "kanagawa",
+	priority = 1001,
+	config = function()
+		vim.cmd.colorscheme("kanagawa-wave")
+		vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "grey" })
+		vim.api.nvim_set_hl(0, "LineNr", { fg = "orange" })
+		vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "grey" })
+	end,
 }
