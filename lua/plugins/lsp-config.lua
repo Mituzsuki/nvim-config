ad6444b28b76ce060f6fbbb5b176c227a97297d1
MatthewHolmesPayble <156634487+MatthewHolmesPayble@users.noreply.github.com>
2025-01-27 22:24:51 +1100

    added eslint to mason auto install.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,7 +10,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "csharp_ls", "powershell_es", "jedi_language_server" },
+        ensure_installed = { "lua_ls", "tsserver", "csharp_ls", "powershell_es", "jedi_language_server", "eslint" },
       })
     end,
   },
