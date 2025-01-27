bbd47727a5da48bc68e6e99e07e307067cb2432b
MatthewHolmesPayble <156634487+MatthewHolmesPayble@users.noreply.github.com>
2025-01-27 23:07:18 +1100

    neo-tree wasn't working properly.

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,23 +1,38 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>:Neotree close<CR>")
-		vim.keymap.set("n", "<C-b>", ":Neotree close<CR>")
-		require("neo-tree").setup({
-			event_handlers = {
-				{
-					event = "file_open_requested",
-					handler = function()
-						require("neo-tree.command").execute({ action = "close" })
-					end,
-				},
-			},
-		})
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>:Neotree close<CR>")
+    vim.keymap.set("n", "<C-b>", ":Neotree close<CR>")
+    require("neo-tree").setup({
+      event_handlers = {
+        {
+          event = "file_open_requested",
+          handler = function()
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+      },
+      enable_diagnostics = false,
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+      default_component_configs = {
+        icon = {
+          folder_closed = "",
+          folder_open = "",
+          folder_empty = "",
+          default = "",
+        },
+      },
+    })
+  end,
 }
